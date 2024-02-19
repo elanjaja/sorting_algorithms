bc3b8815596b67a99aef71eb86d1ec58039525b2
elanajaja <elanjaja01@gmail.com>
2024-02-19 06:31:33 -0800

    edited shell

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,37 +1,34 @@
 #include "sort.h"
+
 /**
- * shell_sort - shell sort algorithm
- * @array: the array
- * @size: the size of the array
- *
- * Description: using the knuth sequence
+ * shell_sort - using the shell_sort to sort the array.
+ * @array: array to be sorted in shell_sort.
+ * @size: size of thr array.
+ * Return: nothing.
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t a, j;
-	int item;
-	size_t gap = 1;
+	size_t a, j, gap_p = 1;
+	int swap_p;
 
 	if (array == NULL || size < 2)
-	{
 		return;
-	}
-	while (gap < (size / 3))
+
+	while (gap_p < size / 3)
+		gap_p = 3 * gap_p + 1;
+
+	while (gap_p > 0)
 	{
-		gap = gap * 3 + 1;
-	}
-	while (gap >= 1)
-	{
-		for (a = gap; a < size; a++)
+		for (a = gap_p; a < size; a++)
 		{
-			for (j = a; j >= gap && array[gap] < array[j - gap]; j -= gap)
+			swap_p = array[a];
+			for (j = a; j >= gap_p && array[j - gap_p] > swap_p; j -= gap_p)
 			{
-				item = array[j];
-				array[j] = array[j - gap];
-				array[j - gap] = item;
+				array[j] = array[j - gap_p];
 			}
+			array[j] = swap_p;
 		}
-		gap = gap / 3;
+		gap_p = (gap_p - 1) / 3;
 		print_array(array, size);
 	}
 }
