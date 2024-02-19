8b08d3a220c0b56752669129f0c440d56e067d98
elanajaja <elanjaja01@gmail.com>
2024-02-19 05:29:37 -0800

    edited insertion files

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,51 +1,57 @@
 #include "sort.h"
 
-/**
- * swap - swap two nodes.
- * @a: first node-- beagining node.
- * @b: second node next node.
- * Return: nothing
- */
-void swap(listint_t *node1, listint_t *node2)
-{
-	if (node1->prev)
-		node1->prev->next = node2;
-	if (node2->next)
-		node2->next->prev = node2;
-	node1->next = node2->next;
-	node2->prev = node1->prev;
-	node1->prev = node2;
-	node2->next = node1;
-}
-
+void swap(listint_t **head, listint_t *node1, listint_t *node2);
 /**
  * insertion_sort_list - perform insertion using insertion_sort_list.
+ *
  * @list: list to be sorted with insertion_sort_list.
+ *
  * Return: nothing
  */
-
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *a, *j;
+	listint_t *forw, *tmp;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	a = (*list)->next;
-	while (a)
+
+	for (forw = (*list)->next; forw && forw->prev; forw = forw->next)
 	{
-		j = a;
-		a = a->next;
-		while (j && j->prev)
+		for (; forw && forw->prev && forw->n < forw->prev->n;
+		     forw = forw->prev)
 		{
-			if (j->prev->n > j->n)
-			{
-				swap(j->prev, j);
-				if (j->prev == NULL)
-					*list = j;
-				print_list((const listint_t *)*list);
-			}
-			else
-				j = j->prev;
+			tmp = forw->prev;
+			swap(list, tmp, forw);
+			print_list(*list);
+			forw = forw->next;
 		}
 	}
+}
+
+/**
+ * swap - swaps nodes one by one.
+ * @head: the head node
+ * @node1: The first node
+ * @node2: the second node
+ *
+ * Return: void
+ */
+void swap(listint_t **head, listint_t *node1, listint_t *node2)
+{
+	listint_t *prev, *next;
+
+	prev = node1->prev;
+	next = node2->next;
+
+	if (prev != NULL)
+		prev->next = node2;
+	else
+		*head = node2;
+
+	node1->prev = node2;
+	node1->next = next;
+	node2->prev = prev;
+	node2->next = node1;
+	if (next)
+		next->prev = node1;
 }
